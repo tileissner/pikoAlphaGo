1aaeb20ad03aa000e8e3bac512b30c3c9c728918
Marcel Fiebig <uas0001884@unknown.fra-uas.de>
2021-03-13 18:31:58 +0100

    Removed unused function

--- components/mcts/search.py
+++ components/mcts/search.py
@@ -28,32 +28,3 @@ class MonteCarloTreeSearch(object):
 
     def randomWinner(self):
         return 1 if random() < 0.5 else -1
-
-
-    def createHistoryStates(self, current_node):
-        parentStates = []
-        current_node.state.pos
-
-        state_t_1 = None
-        state_t_2 = None
-        state_t_3 = None
-
-        if len(current_node.state.pos.board_deltas) > 0:
-            state_t_1 = current_node.state.pos.board - current_node.state.pos.board_deltas[0]
-        if len(current_node.state.pos.board_deltas) > 1:
-            state_t_2 = state_t_1 - current_node.state.pos.board_deltas[1]
-             # state_t_2 = current_node.state.pos-current_node.state.pos.board_deltas[0]-current_node.state.pos.board_deltas[1] #equivalent
-        if len(current_node.state.pos.board_deltas) > 2:
-            state_t_3 = state_t_2 - current_node.state.pos.board_deltas[2]
-
-        if state_t_1 is None:
-            state_t_1 = np.zeros([constants.board_size, constants.board_size], dtype=np.int8)
-        if state_t_2 is None:
-            state_t_2 = np.zeros([constants.board_size, constants.board_size], dtype=np.int8)
-        if state_t_3 is None:
-            state_t_3 = np.zeros([constants.board_size, constants.board_size], dtype=np.int8)
-
-        parentStates.append(state_t_1)
-        parentStates.append(state_t_2)
-        parentStates.append(state_t_3)
-        return parentStates
